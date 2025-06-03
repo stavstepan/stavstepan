2886690b07252804eab6d7a9dd5f6831e83531d8
Stepan <stavstepan@gmail.com>
2025-06-03 10:50:52 +0300

    порядок загрузки стилей для GPS

---
+++ header.php
@@ -0,0 +1,5 @@
+<?
+//ускоряет загрузку основных стилей. дополнительные грузяться в фоне, возможно повысить GPS
+<link rel="stylesheet" href="/dist/base.css">
+<link rel="stylesheet" href="/dist/common.css" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="/dist/home.css" media="print" onload="this.media='all'">
